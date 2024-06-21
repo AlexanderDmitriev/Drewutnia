c2e57d43a437cba6b55b34c262ebe95e7d9f7d0d
AlexanderDmitriev <alex9110077@gmail.com>
2024-06-21 12:53:52 +0200

    20240621

--- src/components/testData/dishes.js
+++ src/components/testData/dishes.js
@@ -1,4 +1,6 @@
 import dish from '../../images/dish.png';
+import kielbasa from '../../images/menu/dania/kielbasa.jpg';
+import kaszanka from '../../images/menu/dania/kaszanka.jpg'
 import pierogi from '../../images/menu/dania/pierogi.jpg';
 import kawaParzona from '../../images/menu/dania/kawaParzona.jpg';
 import coffee from '../../images/menu/dania/coffee.jpg';
@@ -7,18 +9,18 @@ import { v4 as uuidv4 } from 'uuid';
   export const dishesSomethingToEat = [
     {
       id: uuidv4(),
-      image: `${dish}`,
+      image: `${kielbasa}`,
       name: 'Kielbasa z ogniska',
       about:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum tellus nulla, et auctor tortor iaculis et. Nam sed sem nulla. Quisque sit amet ex nec nulla scelerisque commodo. Nulla egestas in tortor ac mattis. Curabitur vel porttitor dui, at rutrum sapien. Suspendisse interdum quam euismod justo bibendum fringilla. ',
+        'Ugotowana za narodowymi tradycjami kielbasa z grilla jest najlepszą przekąską do piwa. Równierz dobrze przyrządzoną kielbasę oferuje bardzo satysfakcjonujący smak i wyjątkowy aromat.',
       price: '10zł',
     },
     {
       id: uuidv4(),
-      image: `${dish}`,
+      image: `${kaszanka}`,
       name: 'Kaszanka z cebulką',
       about:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum tellus nulla, et auctor tortor iaculis et. Nam sed sem nulla. Quisque sit amet ex nec nulla scelerisque commodo. Nulla egestas in tortor ac mattis. Curabitur vel porttitor dui, at rutrum sapien. Suspendisse interdum quam euismod justo bibendum fringilla. ',
+        'Znana i uwielbiana w całym kraju kaszanka. Idealny i prosty posiłek, który serdecznie przyrządzony na patelni z cabulą. Szybka i pyszna kolacja po ciężkim dniu. Mianowicie: kaszy gryczanej, krwi wieprzowej oraz  podrobów takich jak wątróbka, płuca czy ozór. Smakuje jak w domu. Smacznego!',
       price: '10zł',
     },
     {
