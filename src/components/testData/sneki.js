66c5a88d8292c033510e74e2f10e6ad3bdeb5743
AlexanderDmitriev <alex9110077@gmail.com>
2024-07-06 00:01:06 +0200

    20240705

--- src/components/testData/sneki.js
+++ src/components/testData/sneki.js
@@ -1,6 +1,7 @@
-import dish from '../../images/dish.png';
+//import dish from '../../images/dish.png';
 import orzeszki from '../../images/menu/sneki/felix-orzeszki-ziemne-smazone-i-solone-140-g.jpg';
 import paluszki from '../../images/menu/sneki/paszuki-klasyczne.png';
+import chipsy from '../../images/menu/sneki/chipsy.jpg';
 import { v4 as uuidv4 } from 'uuid';
 
   export const sneki = [
@@ -9,15 +10,15 @@ import { v4 as uuidv4 } from 'uuid';
       image: `${paluszki}`,
       name: 'Paluszki klasyczne',
       about:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum tellus nulla, et auctor tortor iaculis et. Nam sed sem nulla. Quisque sit amet ex nec nulla scelerisque commodo. Nulla egestas in tortor ac mattis. Curabitur vel porttitor dui, at rutrum sapien. Suspendisse interdum quam euismod justo bibendum fringilla. ',
+        'Сhrupiąca przekąska z wyrazistym. Jest idealne na każdą okazję jak dla dzieci, tak i dla dorosłych. Produkty Lajkonik wypiekane w Skawinie pod Krakowem.',
       price: '10zł',
     },
     {
       id: uuidv4(),
-      image: `${dish}`,
+      image: `${chipsy}`,
       name: 'Chipsy (musztrda, czosnek, paprika, chronowe lub solone)',
       about:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum tellus nulla, et auctor tortor iaculis et. Nam sed sem nulla. Quisque sit amet ex nec nulla scelerisque commodo. Nulla egestas in tortor ac mattis. Curabitur vel porttitor dui, at rutrum sapien. Suspendisse interdum quam euismod justo bibendum fringilla. ',
+        'Popularna przekąska w postaci cienkich, osuszonych plastrów ziemniaków z różnymi smakami. Zawsze są dobrym dodatkiem do każdej imprezy czy spotkania towarzyskiego.',
       price: '10zł',
     },
     {
@@ -25,7 +26,7 @@ import { v4 as uuidv4 } from 'uuid';
       image: `${orzeszki}`,
       name: 'Orzeszki ziemne solone',
       about:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum tellus nulla, et auctor tortor iaculis et. Nam sed sem nulla. Quisque sit amet ex nec nulla scelerisque commodo. Nulla egestas in tortor ac mattis. Curabitur vel porttitor dui, at rutrum sapien. Suspendisse interdum quam euismod justo bibendum fringilla. ',
+        'Orzeszki ziemne smażone są smaczną przekąską, po którą każdy chętnie sięga. Polecamy do piwa  wszystkim osobom, lubiącym przekąski na słono.',
       price: '10zł',
     },
   ];
